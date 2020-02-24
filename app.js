9adc053cad20de87bca08ed86a33a5dca06c5bff
Felix Bade <flix.bade@gmail.com>
2020-02-24 22:40:23 +0200

    Add elements with directional commands

--- app.js
+++ app.js
@@ -19,8 +19,12 @@ const nodeTypesToEmojis = {
 };
 
 
-let cursorPosition = 0;
 let editMode = 0;
+
+
+
+// Keyboard mapping
+
 var pressedKeys = {};
 
 window.addEventListener("load", function() {
@@ -30,40 +34,87 @@ window.addEventListener("load", function() {
     document.addEventListener("keydown", function(event) {
         pressedKeys[event.code] = true;
 
-        if ((pressedKeys.MetaLeft || pressedKeys.MetaRight) && (pressedKeys.AltLeft || pressedKeys.AltRight)) {
-            if (event.code === "ArrowDown") {
-                swapDown();
-                event.preventDefault();
-            } else if (event.code === "ArrowUp") {
-                swapUp();
-                event.preventDefault();
-            } else if (event.code === "ArrowRight") {
-                increaseIndentUnderCursor();
-                event.preventDefault();
-            } else if (event.code === "ArrowLeft") {
-                decreaseIndentUnderCursor();
-                event.preventDefault();
-            }
-        }
-
         if (editMode === 1) {
             if (event.code === "Enter") {
                 endEditMode();
             }
-        } else {
-            if (event.code === "Enter") {
-                startEditMode();
-            } else if (event.code in keysToNodeTypes) {
-                setTypeUnderCursor(keysToNodeTypes[event.code]);
+            
+        } else if ((pressedKeys.MetaLeft || pressedKeys.MetaRight) && (pressedKeys.AltLeft || pressedKeys.AltRight)) {
+            if (event.code === "ArrowDown") {
+                swapDown();
                 updateVisibleList();
-            } else if (event.code === "Backspace") {
-                removeElementUnderCursor();
-            } else if (event.code === "ArrowDown") {
-                cursorDown();
                 event.preventDefault();
+
             } else if (event.code === "ArrowUp") {
-                cursorUp();
+                swapUp();
+                updateVisibleList();
                 event.preventDefault();
+
+            } else if (event.code === "ArrowRight") {
+                increaseIndentUnderCursor();
+                updateVisibleList();
+                event.preventDefault();
+
+            } else if (event.code === "ArrowLeft") {
+                decreaseIndentUnderCursor();
+                updateVisibleList();
+                event.preventDefault();
+            }
+
+        } else {
+
+            let pressedTypeKeys = Object.keys(pressedKeys)
+            .filter(key => pressedKeys[key] === true)
+            .filter(key => key in keysToNodeTypes);
+
+            if (pressedTypeKeys.length == 1) {
+                const type = keysToNodeTypes[pressedTypeKeys[0]];
+
+                if (event.code === "ArrowDown") {
+                    addNodeDown(type);
+                    updateVisibleList();
+                    startEditMode();
+                    event.preventDefault();
+
+                } else if (event.code === "ArrowUp") {
+                    addNodeUp(type);
+                    updateVisibleList();
+                    startEditMode();
+                    event.preventDefault();
+
+                } else if (event.code === "ArrowRight") {
+                    addNodeRight(type);
+                    updateVisibleList();
+                    startEditMode();
+                    event.preventDefault();
+
+                } else if (event.code === "ArrowLeft") {
+                    addNodeLeft(type);
+                    updateVisibleList();
+                    startEditMode();
+                    event.preventDefault();
+                }
+            }
+
+            if (pressedTypeKeys.length == 0) {
+
+                if (event.code === "Enter") {
+                    startEditMode();
+
+                } else if (event.code === "Backspace") {
+                    removeElementUnderCursor();
+                    updateVisibleList();
+
+                } else if (event.code === "ArrowDown") {
+                    cursorDown();
+                    updateVisibleList();
+                    event.preventDefault();
+
+                } else if (event.code === "ArrowUp") {
+                    cursorUp();
+                    updateVisibleList();
+                    event.preventDefault();
+                }
             }
 
         }
@@ -72,14 +123,26 @@ window.addEventListener("load", function() {
 
     document.addEventListener("keyup", function(event) {
         pressedKeys[event.code] = false;
+
+        if (editMode === 0) {
+            if (event.code in keysToNodeTypes) {
+                setTypeUnderCursor(keysToNodeTypes[event.code]);
+                updateVisibleList();
+            }
+        }
     });
 });
+
+
+
+// Rendering
 
 function startEditMode() {
     editMode = 1;
     document.getElementById("status").innerText = "editing";
 
     const element = getCursorElement();
+    console.log(element);
     const text = getTextUnderCursor();
 
     const span = element.getElementsByTagName("span")[0];
@@ -97,6 +160,11 @@ function getCursorElement() {
     return document.getElementsByClassName("cursor-node")[0];
 }
 
+function getTextUnderCursor() {
+    let previous = getSavedElements();
+    return previous[cursorPosition].text;
+}
+
 function endEditMode() {
     editMode = 0;
     document.getElementById("status").innerText = "viewing";
@@ -104,49 +172,6 @@ function endEditMode() {
     const newText = inputElement.value;
     setTextUnderCursor(newText);
     updateVisibleList();
-}
-
-
-
-function readCursorPositionFromHash() {
-    const position = parseInt(document.location.hash.substr(1));
-    if (!isNaN(position)) {
-        cursorPosition = position;
-    } else {
-        cursorPosition = 0;
-    }
-    clampAndSaveCursorPosition();
-    updateVisibleList();
-}
-
-window.addEventListener("hashchange", readCursorPositionFromHash, false);
-
-function cursorDown() {
-    cursorPosition += 1;
-    clampAndSaveCursorPosition();
-    updateVisibleList();
-}
-
-function cursorUp() {
-    cursorPosition -= 1;
-    clampAndSaveCursorPosition();
-    updateVisibleList();
-}
-
-function clampAndSaveCursorPosition() {
-    cursorPosition = Math.min(getSavedElements().length - 1, cursorPosition);
-    cursorPosition = Math.max(0, cursorPosition);
-    document.location.hash = cursorPosition;
-    console.log(document.location.hash);
-}
-
-
-
-function addNode(type) {
-    if (type in nodeTypesToEmojis) {
-        addEmptyElementAfterIndex(type, cursorPosition);
-    }
-    console.warn("Unrecognized node type: " + type);
 }
 
 function updateVisibleList() {
@@ -172,28 +197,20 @@ function updateVisibleList() {
     });
 }
 
-function getSavedElements() {
-    let data = loadData();
-    if (data === null) {
-        data = [];
-    }
-    return data;
+
+
+// Actions
+
+function setTextUnderCursor(text) {
+    let previous = getSavedElements();
+    previous[cursorPosition].text = text;
+    saveData(previous);
 }
 
 function setTypeUnderCursor(type) {
     let previous = getSavedElements();
     previous[cursorPosition].type = type;
     saveData(previous);
-    updateVisibleList();
-}
-
-function addEmptyElementAfterIndex(type, index) {
-    let element = {"type": type, "text": "", "indentation": 0};
-    let previous = getSavedElements();
-    previous.splice(index+1, 0, element);
-    saveData(previous);
-    cursorDown();
-    updateVisibleList();
 }
 
 function removeElementUnderCursor() {
@@ -201,8 +218,36 @@ function removeElementUnderCursor() {
     previous.splice(cursorPosition, 1);
     saveData(previous);
     cursorUp();
-    updateVisibleList();
 }
+
+// Adding
+
+function addNodeDown(type) {
+    const indentation = getIndentationAtCursor();
+    addEmptyElementAtIndex(type, cursorPosition+1, indentation);
+    cursorDown();
+}
+
+function addNodeUp(type) {
+    const indentation = getIndentationAtCursor();
+    addEmptyElementAtIndex(type, cursorPosition, indentation);
+    cursorUp();
+}
+
+function addNodeRight(type) {
+    const indentation = getIndentationAtCursor();
+    addEmptyElementAtIndex(type, cursorPosition+1, indentation+1);
+    cursorDown();
+}
+
+function addNodeLeft(type) {
+    const indentation = getIndentationAtCursor();
+    increaseIndentUnderCursor();
+    addEmptyElementAtIndex(type, cursorPosition, indentation);
+    cursorUp();
+}
+
+// Grab
 
 function swapDown() {
     swap(cursorPosition, cursorPosition + 1);
@@ -212,35 +257,11 @@ function swapUp() {
     swap(cursorPosition - 1, cursorPosition);
 }
 
-function swap(a, b) {
-    let previous = getSavedElements();
-    if (a < 0 || a >= previous.length || b < 0 || b >= previous.length) {
-        return;
-    }
-    let temp = previous[a];
-    previous[a] = previous[b];
-    previous[b] = temp;
-    saveData(previous);
-}
-
-function setTextUnderCursor(text) {
-    let previous = getSavedElements();
-    previous[cursorPosition].text = text;
-    saveData(previous);
-    updateVisibleList();
-}
-
-function getTextUnderCursor() {
-    let previous = getSavedElements();
-    return previous[cursorPosition].text;
-}
-
 function increaseIndentUnderCursor(text) {
     let previous = getSavedElements();
     let curr = previous[cursorPosition].indentation || 0;
     previous[cursorPosition].indentation = curr + 1;
     saveData(previous);
-    updateVisibleList();
 }
 
 function decreaseIndentUnderCursor(text) {
@@ -248,10 +269,91 @@ function decreaseIndentUnderCursor(text) {
     let curr = previous[cursorPosition].indentation || 0;
     previous[cursorPosition].indentation = Math.max(0, curr - 1);
     saveData(previous);
-    updateVisibleList();
+}
+
+// Action helpers
+
+function getIndentationAtCursor() {
+    let previous = getSavedElements();
+    return previous[cursorPosition].indentation;
+}
+
+function swap(a, b) {
+    let previous = getSavedElements();
+    if (a < 0 || a >= previous.length || b < 0 || b >= previous.length) {
+        return;
+    }
+
+    let temp = previous[a];
+    previous[a] = previous[b];
+    previous[b] = temp;
+
+    if (cursorPosition == a) {
+        cursorPosition = b;
+        clampAndSaveCursorPosition();
+    } else if (cursorPosition == b) {
+        cursorPosition = a;
+        clampAndSaveCursorPosition();
+    }
+    saveData(previous);
+}
+
+function addEmptyElementAtIndex(type, index, indentation) {
+    let element = {"type": type, "text": "", "indentation": indentation};
+    let previous = getSavedElements();
+    previous.splice(index, 0, element);
+    saveData(previous);
+    if (index <= cursorPosition) {
+        cursorDown();
+    }
 }
 
 
+
+// Cursor
+
+let cursorPosition = 0;
+
+function cursorDown() {
+    cursorPosition += 1;
+    clampAndSaveCursorPosition();
+}
+
+function cursorUp() {
+    cursorPosition -= 1;
+    clampAndSaveCursorPosition();
+}
+
+function clampAndSaveCursorPosition() {
+    cursorPosition = Math.min(getSavedElements().length - 1, cursorPosition);
+    cursorPosition = Math.max(0, cursorPosition);
+    document.location.hash = cursorPosition;
+    console.log(document.location.hash);
+}
+
+function readCursorPositionFromHash() {
+    const position = parseInt(document.location.hash.substr(1));
+    if (!isNaN(position)) {
+        cursorPosition = position;
+    } else {
+        cursorPosition = 0;
+    }
+    clampAndSaveCursorPosition();
+}
+
+window.addEventListener("hashchange", readCursorPositionFromHash, false);
+
+
+
+// Element storage
+
+function getSavedElements() {
+    let data = loadData();
+    if (data === null) {
+        data = [];
+    }
+    return data;
+}
 
 const localStorageKey = "todoapp-data";
 
